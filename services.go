a4f7135f12a7e8ff4931dbc17bfae86f86033b3c
jfoster <jacobfoster@pm.me>
2020-03-04 21:41:50 +0000

    Better naming and error handling in vehicle service

--- services.go
+++ services.go
@@ -8,25 +8,27 @@ import (
 func newVehiclesService(c *Client) *VehiclesService {
 	return &VehiclesService{
 		BasePath: "vehicles",
-		c:        c,
+		client:   c,
 	}
 }
 
 type VehiclesService struct {
 	BasePath string
 
-	c *Client
+	client *Client
 }
 
 func (vs *VehiclesService) GetVehicle(reg string) (v *Vehicle, err error) {
 	v = new(Vehicle)
 
-	b, _ := json.Marshal(VehicleRequest{
+	b, err := json.Marshal(VehicleRequest{
 		RegistrationNumber: reg,
 	})
+	if err != nil {
+		return nil, err
+	}
 
-	req := vs.c.NewRequest("POST", vs.BasePath, bytes.NewBuffer(b))
-
+	req := vs.client.NewRequest("POST", vs.BasePath, bytes.NewBuffer(b))
 	_, err = req.Do(v)
 	if err != nil {
 		return nil, err
