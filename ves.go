4832ada7181c6c1c7ef2425c4a3991da052174ce
jfoster <jacobfoster@pm.me>
2020-03-04 21:39:10 +0000

    Add uat ves url constant

--- ves.go
+++ ves.go
@@ -3,4 +3,6 @@ package ves
 const (
 	VESAPIUrl     = "https://driver-vehicle-licensing.api.gov.uk/vehicle-enquiry"
 	VESAPIVersion = 1
+
+	UATVESAPIUrl = "https://uat.driver-vehicle-licensing.api.gov.uk/vehicle-enquiry"
 )
