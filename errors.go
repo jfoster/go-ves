fa933c2daa1fd77ed44f0866189ac1b5477f06d5
jfoster <jacobfoster@pm.me>
2020-03-14 13:21:39 +0000

    omit empty error details

--- errors.go
+++ errors.go
@@ -1,18 +1,27 @@
 package ves
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 type ErrorResponse struct {
 	Errors []Error `json:"errors"`
 }
 
 type Error struct {
-	Status string `json:"status"`
-	Code   string `json:"code"`
+	Status Status `json:"status,omitempty"`
+	Code   Status `json:"code,omitempty"`
 	Title  string `json:"title"`
-	Detail string `json:"detail"`
+	Detail string `json:"detail,omitempty"`
+}
+
+type Status string
+
+func (s Status) Int() (int, error) {
+	return strconv.Atoi(string(s))
 }
 
 func (e Error) Error() string {
-	return fmt.Sprintf("Error %s %s: %s", e.Code, e.Title, e.Detail)
+	return fmt.Sprintf("Error %s (%s) %s: %s", e.Status, e.Code, e.Title, e.Detail)
 }
